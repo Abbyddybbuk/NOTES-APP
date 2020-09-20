21fc59eed16cdb726329e36aa91be8e8320de70b
I066841 <a.kulshreshtha@sap.com>
2020-09-21 00:48:45 +0530

    Process-argv/Yargs

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
-// const fs = require('fs')
-// fs.writeFileSync('notes.txt', 'My Name is Abhijeet Kulshreshtha')
-// fs.appendFileSync('notes.txt', '. I live in Bangalore')
-
-// const add = require('./utils.js')
-// const sum = add(4, 5)
-// console.log(sum)
-const validator=require('validator')
-const chalk=require('chalk')
+const validator = require('validator')
+const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 console.log(chalk.yellow.bold.inverse(getNotes()))
 console.log(chalk.red(validator.isEmail('abhijeet.abap%cbc.com')))
 console.log(chalk.blue.bold(validator.isURL('www.sap.com')))
+
+// Customize your app version
+yargs.version('1.1.0')
+
+// Create Command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: () => console.log('Adding a new note!')
+})
+
+// Create Remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: () => console.log('Removing a note!')
+})
+
+// Create List command
+yargs.command({
+    command: 'list',
+    describe: 'Listing all the notes',
+    handler: () => console.log('Listing all the notes!')
+})
+
+// Create Read command
+yargs.command({
+    command: 'read',
+    describe: 'Read all the notes',
+    handler: () => console.log('Reading all the notes!')
+})
+
+console.log(yargs.argv)
