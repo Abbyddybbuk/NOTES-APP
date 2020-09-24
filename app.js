333b885cca9f04749b4e181184e4f285f821ab15
I066841 <a.kulshreshtha@sap.com>
2020-09-25 00:56:47 +0530

    How to add a note

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const validator = require('validator')
 const chalk = require('chalk')
 const yargs = require('yargs')
-const getNotes = require('./notes.js')
-console.log(chalk.yellow.bold.inverse(getNotes()))
-console.log(chalk.red(validator.isEmail('abhijeet.abap%cbc.com')))
-console.log(chalk.blue.bold(validator.isURL('www.sap.com')))
+const notes = require('./notes.js')
+// console.log(chalk.yellow.bold.inverse(getNotes()))
+// console.log(chalk.red(validator.isEmail('abhijeet.abap%cbc.com')))
+// console.log(chalk.blue.bold(validator.isURL('www.sap.com')))
 
 // Customize your app version
 yargs.version('1.1.0')
@@ -13,7 +13,21 @@ yargs.version('1.1.0')
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: () => console.log('Adding a new note!')
+    builder: {
+        title: {
+           describe: 'Note Title',
+           demandOption: true,
+           type: 'string'
+        },
+        body: {
+            describe: 'Note Body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => {
+        notes.addNote(argv.title, argv.body)
+    } 
 })
 
 // Create Remove command
@@ -37,4 +51,5 @@ yargs.command({
     handler: () => console.log('Reading all the notes!')
 })
 
-console.log(yargs.argv)
+// console.log(yargs.argv)
+yargs.parse()
--- notes.js
+++ notes.js
@@ -1,3 +1,45 @@
+const fs = require('fs')
+
 const getNotes = () => 'Your notes are ready'
 
-module.exports = getNotes
+const addNote = (title, body) => {
+    const notes = loadNotes()
+    console.log(notes)
+    const duplicateNotes = notes.filter((note) => {
+         return note.title === title
+    })
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+         })
+         
+         saveNotes(notes)      
+    } else {
+       console.log('Note already exists!')
+    }
+}
+
+const saveNotes = (notes) => {
+   const dataJSON = JSON.stringify(notes)
+   fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')        
+        // Convert the read JSON from machine format to readable JSON format
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (error) {
+        return []
+    }
+}
+
+
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+}
